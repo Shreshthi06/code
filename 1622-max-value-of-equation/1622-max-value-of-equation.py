e927be7bef7e15d6a468e93d48ea708d50afb696
Shreshthi <156869895+Shreshthi06@users.noreply.github.com>
2025-03-11 22:56:52 +0530

    Time: 72 ms (99.34%), Space: 55.4 MB (87.42%) - LeetHub

--- 1622-max-value-of-equation/1622-max-value-of-equation.py
+++ 1622-max-value-of-equation/1622-max-value-of-equation.py
@@ -3,22 +3,21 @@ from collections import deque
 class Solution:
     def findMaxValueOfEquation(self, points, k):
         max_value = float('-inf')
-        dq = deque()  # Stores (xi, yi - xi)
+        dq = deque() 
 
         for xj, yj in points:
-            # Remove points that are out of range (xi < xj - k)
+           
             while dq and dq[0][0] < xj - k:
                 dq.popleft()
             
-            # Calculate max equation value if deque is not empty
+           
             if dq:
                 max_value = max(max_value, yj + xj + dq[0][1])
             
-            # Remove elements from the back that are not useful
+            
             while dq and dq[-1][1] <= yj - xj:
                 dq.pop()
-            
-            # Add current point to the deque
+        
             dq.append((xj, yj - xj))
         
         return max_value
