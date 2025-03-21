713b93aefb716a460ab58131e801a0e551a6b92a
Shreshthi <156869895+Shreshthi06@users.noreply.github.com>
2025-03-21 23:07:43 +0530

    Time: 0 ms (100%), Space: 8.7 MB (5.12%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        // if (divisor == 0) {
-        //     throw std::runtime_error("Division by zero is not allowed");
-        // }
-
+        
         
         if (dividend == INT_MIN && divisor == -1) {
             return INT_MAX; 
